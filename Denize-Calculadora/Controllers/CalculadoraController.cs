83283428f11324eadf41844625422091988b6719
Denize Bassi Figueiredo <denize.f.bassi@gmail.com>
2022-12-02 08:30:49 -0300

    Add project files.

---
+++ Denize-Calculadora/Controllers/CalculadoraController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Denize_Calculadora.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Somar(int a, int b)
+        {
+            ViewData["result"] = a+b;
+            return View();
+        }
+        public IActionResult Subtrair(int a, int b)
+        {
+            ViewData["result"] = a - b;
+            return View();
+        }
+        public IActionResult Dividir(int a, int b)
+        {
+            if(a > 0 && b > 0)
+            {
+                ViewData["result"] = a / b;
+            }
+            ViewData["result"] = 0;
+
+            return View();
+        }
+        public IActionResult Multiplicar(int a, int b)
+        {
+            ViewData["result"] = a * b;
+            return View();
+        }
+    }
+}
---
+++ Denize-Calculadora/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Denize_Calculadora.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
